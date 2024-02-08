3270a6557695a45a3ff9013833317a912ec7e95e
kunal.zanke <kunal.zanke@globallogic.com>
2024-02-08 11:54:27 +0530

    log form data

--- src/Components/FormExample.js
+++ src/Components/FormExample.js
@@ -1,27 +1,32 @@
 // App.js
-import React from 'react';
+import React, { useState } from 'react';
 import AddressForm from './AddressForm';
 import { frValidationSchema, gbValidationSchema } from './Schema';
 
 const FormExample = () => {
+  const [gb, setGb] = useState(null);
+  const [fr, setFr] = useState(null);
+
   const handleGbSubmit = (data) => {
     console.log('GB Address:', data);
-    alert(JSON.stringify(data))
+    setGb(data);
   };
 
   const handleFrSubmit = (data) => {
     console.log('FR Address:', data);
-    alert(JSON.stringify(data))
+    setFr(data);
   };
 
   return (
     <div className="flex justify-around items-center h-screen">
       <div className="bg-white rounded-lg shadow-md p-8 w-1/4">
         <h1 className="text-2xl font-bold mb-4">GB Address</h1>
+        {gb && <pre>{JSON.stringify(gb, null, 2)}</pre>}
         <AddressForm validationSchema={gbValidationSchema} onSubmit={handleGbSubmit} />
         </div>
         <div className="bg-white rounded-lg shadow-md p-8 w-1/4">
         <h1 className="text-2xl font-bold mt-8 mb-4">FR Address</h1>
+        {fr && <pre>{JSON.stringify(fr, null, 2)}</pre>}
         <AddressForm validationSchema={frValidationSchema} onSubmit={handleFrSubmit} type='FR'/>
         </div>
     </div>
